75922b41c05ca5618585e376d05c3964bedb87d4
Oleksandr Mykolaichuk <alexmix3000@gmail.com>
2020-05-07 19:44:56 +0200

    added allowed param

--- src/main/java/com/alm/research/gov/paperwork/apigateway/filters/AuthFilter.java
+++ src/main/java/com/alm/research/gov/paperwork/apigateway/filters/AuthFilter.java
@@ -5,14 +5,21 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.RedirectToGatewayFilterFactory;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.style.ToStringCreator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
@@ -32,6 +39,11 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
+
+            //bypass allowed urls
+            if (config.getAllowed().contains(request.getPath().toString())) {
+                return chain.filter(exchange);
+            }
 
             return this.webClientBuilder
                     .baseUrl(authValidationUrl)
@@ -60,5 +72,7 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
 
     @Data
     @NoArgsConstructor
-    public static class Config {}
+    public static class Config {
+        private List<String> allowed;
+    }
 }
