e69dc5b4c33e00bf55a72d238840e569eda597d0
Oleksandr Mykolaichuk <alexmix3000@gmail.com>
2020-05-10 00:01:00 +0200

    added api docs bypass

--- src/main/java/com/alm/research/gov/paperwork/apigateway/filters/AuthFilter.java
+++ src/main/java/com/alm/research/gov/paperwork/apigateway/filters/AuthFilter.java
@@ -5,20 +5,19 @@ import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.cloud.gateway.filter.factory.AbstractNameValueGatewayFilterFactory;
-import org.springframework.cloud.gateway.filter.factory.RedirectToGatewayFilterFactory;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.style.ToStringCreator;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.PathContainer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
 import reactor.core.publisher.Flux;
 
-import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -40,8 +39,15 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
         return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
 
+            String path = request.getPath().toString();
+
             //bypass allowed urls
-            if (config.getAllowed().contains(request.getPath().toString())) {
+            if (config.getAllowed().contains(path)) {
+                return chain.filter(exchange);
+            }
+
+            //allow api-docs
+            if (this.matchAPIDocsPath(path)) {
                 return chain.filter(exchange);
             }
 
@@ -68,6 +74,21 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
                         return exchange.getResponse().writeWith(body);
                     });
         };
+    }
+
+    private boolean matchAPIDocsPath(String path) {
+        List<PathPattern> pathPatterns = new ArrayList<>();
+        List<String> apiDocsUrlPatters = List.of("/*/swagger-ui/**", "/*/v3/api-docs/**", "/*/api-docs/**", "/*/webjars/**");
+
+        PathPatternParser pathPatternParser = new PathPatternParser();
+        pathPatternParser.setMatchOptionalTrailingSeparator(true);
+
+        apiDocsUrlPatters.forEach(apiDocsUrl -> pathPatterns.add(pathPatternParser.parse(apiDocsUrl)));
+
+        return pathPatterns.stream().anyMatch(pathPattern -> {
+            PathContainer pathContainer = PathContainer.parsePath(path);
+            return pathPattern.matches(pathContainer);
+        });
     }
 
     @Data
